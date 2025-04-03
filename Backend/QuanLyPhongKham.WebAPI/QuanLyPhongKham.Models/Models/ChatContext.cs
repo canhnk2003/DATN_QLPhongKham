8d8ae9ce13be68e3def84dacb43b488563378101
canhnk2003 <nguyenkhaccanh26022003@gmail.com>
2025-04-03 23:44:03 +0700

    Update lần 3: Đã tích hợp được AI vào chatbot tư vấn Bác sĩ, dịch vụ, đặt lịch phù hợp

--- Backend/QuanLyPhongKham.WebAPI/QuanLyPhongKham.Models/Models/ChatContext.cs
+++ Backend/QuanLyPhongKham.WebAPI/QuanLyPhongKham.Models/Models/ChatContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyPhongKham.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,17 @@ namespace QuanLyPhongKham.Models.Models
     // Lớp lưu ngữ cảnh cuộc trò chuyện
     public class ChatContext
     {
-        public string LastTopic { get; set; } = "";
-        public string LastSpecialization { get; set; } = "";
+        public Guid? PatientId { get; set; }
+        public string? PatientName { get; set; }
+        public string? PatientPhone { get; set; }
+        public string? PatientEmail { get; set; }
+        public DateTime? AppointmentDate { get; set; }
+        public string? AppointmentTime { get; set; }
+        public Guid? DoctorId { get; set; }
+        public string? DoctorName { get; set; }
+        public Guid? ServiceId { get; set; }
+        public string? ServiceName { get; set; }
+        public string? Reason { get; set; }
+        public bool IsReadyForBooking { get; set; } = false;
     }
 }
