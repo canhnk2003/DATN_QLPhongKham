dca92fc3196ab7e448cef042ac44ef70ad8778a9
canhnk2003 <nguyenkhaccanh26022003@gmail.com>
2025-04-24 13:40:59 +0700

    Update lần 14: Tạo UI Trang chủ Admin, Bác sĩ, 1 phần backen Home Admin

---
+++ Backend/QuanLyPhongKham.WebAPI/QuanLyPhongKham.Models/Models/ClinicInfor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongKham.Models.Models
+{
+    public class ClinicInfor
+    {
+        public int SumAppointment { get; set; }
+        public int SumAppointmentSuccess { get; set; }
+        public int SumAppointmentCancel { get; set; }
+        public int SumPatient { get; set; }
+    }
+}
