ed4b840bf8510d72893e216490abbf282a11f9fb
canhnk2003 <nguyenkhaccanh26022003@gmail.com>
2025-05-06 22:44:05 +0700

    Update lần 22: Sửa một vài chi tiết nhỏ liên quan đến avatar

--- Backend/QuanLyPhongKham.WebAPI/QuanLyPhongKham.Data/Class1.cs
+++ Backend/QuanLyPhongKham.WebAPI/QuanLyPhongKham.Data/Class1.cs
@@ -1,7 +1,11 @@
-﻿namespace QuanLyPhongKham.Data
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using QuanLyPhongKham.Models.Models;
+using System;
+
+namespace QuanLyPhongKham.Data
 {
     public class Class1
     {
-
+        
     }
 }
